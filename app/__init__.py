f1596a7d01f163d2b796acb886614d39c0cdfa9e
dby <17337995232@163.com>
2025-06-23 19:51:46 +0800

    用户中心

--- app/__init__.py
+++ app/__init__.py
@@ -57,6 +57,7 @@ def create_app():
     from .routes.leaf_sheath_identify_file import leaf_sheath_identify_file_ns
     from .routes.ym_analyzer import ym_analyzer_ns
     from .routes.ym_last_analyzer import ym_last_analyzer_ns
+    from .routes.statistics import statistics_ns
 
     api.add_namespace(user_ns, path='/auth')
     api.add_namespace(chr_identify_ns, path='/chr_identify')
@@ -70,6 +71,7 @@ def create_app():
     api.add_namespace(leaf_sheath_identify_file_ns, path='/leaf_sheath_identify_file')
     api.add_namespace(ym_analyzer_ns, path='/ym_analyzer')
     api.add_namespace(ym_last_analyzer_ns, path='/ym_last_analyzer')
+    api.add_namespace(statistics_ns, path='/statistics')
 
     # 添加根路由
     @app.route('/')
